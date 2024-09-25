6fae56c8674f702f6f3074dc98dd685d36abf586
Stefan Peters <Stefan.Peters@gbv.de>
2024-09-25 13:32:47 +0200

    Update linting to ESLint v9 and new rules

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import gbv from "eslint-config-gbv"
+import vue from "eslint-config-gbv/vue"
+
+export default [
+  ...gbv,
+  ...vue,
+  {
+    rules: {
+      "vue/multi-word-component-names": "off",
+    },
+  },
+]
