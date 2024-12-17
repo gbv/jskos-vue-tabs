1eb98d2b79afcba0365493c5062c650c66d4aa68
Stefan Peters <Stefan.Peters@gbv.de>
2024-12-17 13:21:35 +0100

    Update dependencies

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, "src/index.js"),
       name: "JskosVueTabs",
+      cssFileName: "style",
     },
     rollupOptions: {
       external: ["vue"],
